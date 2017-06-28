66017042d750284f3fa6413843b99bb99ab5cef1
jhvk38 <jhahitesh68@gmail.com>
2017-06-28 12:07:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,11 @@ console.log('Loaded!');
 var element =document.getElementById('main-text');
 
 element.innerHTML="New ValuE";
+
+//Move the image
+
+var img = document.getElementById('madi');
+
+img.onclick=function(){
+    img.style.marginLeft'100px';
+};
