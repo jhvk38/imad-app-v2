88d2915deada59f5fc5993eaa3a0916462e5a2c5
jhvk38 <jhahitesh68@gmail.com>
2017-07-06 23:45:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,14 @@
-var x;
-document.getElementById("line").innerHTML="heello00";
-x=document.getElementById("name").innerHTML;
-document.getElementById("line").innerHTML=x;
+var button=document.getElementById("counter");
+button.onclick=function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest,DONE){
+            if(request.status===200){
+                var counter=request.responseText;
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://jhvk38.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
