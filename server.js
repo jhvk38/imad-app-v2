76d6c7073c06877f7e7d813e16d7d22b309e5271
jhvk38 <jhahitesh68@gmail.com>
2017-07-08 13:35:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/tryout.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'tryout.html'));
+});
+
 var counter=0;
 app.get('/counter',function(req,res){
    counter++;
