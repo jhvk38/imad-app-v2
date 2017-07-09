76fb07055b8771ae291433ee7885aa3a58a085d0
jhvk38 <jhahitesh68@gmail.com>
2017-07-09 23:36:21 +0530

    [imad-console] Updates ui/mainmovie.js

--- ui/mainmovie.js
+++ ui/mainmovie.js
@@ -1,6 +1,11 @@
 $(document).ready(()=>{
    $('#searchForm').on('submit',(e)=>{
-       console.log($('#searchText').val());
+       let searchText=($('#searchText').val());
+       getMovies(searchText);
        e.preventDefault();
    });
 });
+
+function getMovies(searchText){
+    console.log(searchText);
+}
