291fecc8d349e8767fd69e5c9c9e26d3a84de612
jhvk38 <jhahitesh68@gmail.com>
2017-07-20 14:56:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,7 +125,9 @@ app.get('/articles/:articleName', function(req,res){
     });
 });
 
-
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
 
 app.get('/ui/style2.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style2.css'));
